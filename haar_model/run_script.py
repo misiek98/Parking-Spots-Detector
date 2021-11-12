0ac9538fb55b00a4c1d1901102a0a3c22bd79688
misiek98 <michasiek098@gmail.com>
2021-11-12 21:49:47 +0100

    .

---
+++ haar_model/run_script.py
@@ -0,0 +1,104 @@
+import os
+import cv2
+import shutil
+from tools.data_processing import Point, data_processing_methods
+
+
+listOfFolders = [os.path.join(os.getcwd(), 'haar_model', 'data'),
+                 os.path.join(os.getcwd(), 'haar_model', 'neg'),
+                 os.path.join(os.getcwd(), 'haar_model', 'pos'),
+                 os.path.join(os.getcwd(), 'haar_model', 'results')]
+
+for dir in listOfFolders:
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+listOfPoints = []
+
+
+def get_position(event, x, y, flags, param):
+    '''
+    Function get_position does a few things. When you double click left mouse button function will draw a small circle at this point and add their coordinates (saved in class Point) to the listOfPoints list.
+
+    event: induced action
+    x: x coordinate
+    y: y coordinate
+    flags and params were required to this function but weren't used.
+    '''
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img=img, center=(x, y), radius=2,
+                   color=(255, 255, 255), thickness=-1)
+        listOfPoints.append(Point(x=x, y=y))
+
+
+imageSet = os.path.join(
+    os.getcwd(), 'haar_model', 'data')
+
+
+for image in os.listdir(imageSet):
+    imagePath = os.path.join(imageSet, image)
+    img = cv2.imread(imagePath)
+
+    cv2.namedWindow('Parking space')
+    cv2.setMouseCallback('Parking space', get_position)
+
+    while True:
+        cv2.imshow('Parking space', img.copy())
+
+        # by pressing 'n' program process data from listOfPoints, save it to parking_lines.info file and copy image from data to pos folder
+        if cv2.waitKey(1) == ord('n'):
+            if len(listOfPoints) % 2 == 0:
+                print('Correct number of points, creating file...')
+
+                data_processing_methods.prepare_vector_data(
+                    listOfPoints=listOfPoints)
+
+                pathToInfoFile = os.path.join(
+                    os.getcwd(), 'haar_model', 'parking_lines.info')
+
+                posImagePath = os.path.join(
+                    os.getcwd(), 'haar_model', 'pos', image)
+
+                # save processed data to parking_lines.info file
+                with open(pathToInfoFile, 'a') as file:
+                    file.write(f'{posImagePath} {int(len(listOfPoints) / 2)} ')
+
+                    for coordinate in listOfPoints:
+                        file.write(f'{coordinate.x} {coordinate.y} ')
+                    file.write('\n')
+
+                print(f'File was saved: {pathToInfoFile}')
+                listOfPoints.clear()
+
+                # copies image from data folder to pos folder
+                shutil.copyfile(src=imagePath, dst=os.path.join(
+                    os.getcwd(), 'haar_model', 'pos', image))
+
+                break
+
+            else:
+                print('Incorrect number of points, points weren\'t saved')
+                listOfPoints.clear()
+                break
+
+        # by pressing 'c' program cuts out the marked part(s) of the image and saves it to the neg folder.
+        if cv2.waitKey(1) == ord('c'):
+            img = cv2.imread(imagePath)
+            data_processing_methods.save_marked_area(
+                listOfPoints=listOfPoints, img=img)
+            listOfPoints.clear()
+            break
+
+        # by pressing 'd' program displays image with drawn rectangles around marked objects
+        if cv2.waitKey(1) == ord('d'):
+            imageWithRectangles = cv2.imread(imagePath)
+            cv2.imshow('Marked objects',
+                       data_processing_methods.draw_rectangles(listOfPoints=listOfPoints, img=imageWithRectangles))
+
+        # by pressing 'r' program removes last point from listOfPoints and from image
+        if cv2.waitKey(1) == ord('r'):
+            img = cv2.imread(imagePath)
+            listOfPoints.pop()
+            for point in listOfPoints:
+                cv2.circle(img=img, center=(
+                    point.x, point.y), radius=2, color=(255, 255, 255), thickness=-1)
---
+++ haar_model/tools/data_processing.py
@@ -0,0 +1,96 @@
+import os
+import cv2
+
+
+class Point:
+    '''
+    Class Point contains X and Y coordinates.
+
+    x: x coordinate
+    y: y coordinate
+    '''
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class data_processing_methods(Point):
+    '''
+    The methods below allow you to prepare data for a Haar-Cascade algorithm. 
+    '''
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    @staticmethod
+    def prepare_vector_data(listOfPoints: list):
+        '''
+        This method prepares data from listOfPoints to generate a .vec file that will be used to train a Haar Cascade model.
+
+        listOfPoints: list that contains Point objects
+        '''
+
+        for iteration in range(len(listOfPoints)):
+            if iteration % 2 == 1:
+                width = abs(listOfPoints[iteration].x -
+                            listOfPoints[iteration - 1].x)
+
+                height = abs(listOfPoints[iteration].y -
+                             listOfPoints[iteration - 1].y)
+
+                if listOfPoints[iteration - 1].x > listOfPoints[iteration].x:
+                    listOfPoints[iteration - 1].x = listOfPoints[iteration].x
+
+                if listOfPoints[iteration - 1].y > listOfPoints[iteration].y:
+                    listOfPoints[iteration - 1].y = listOfPoints[iteration].y
+
+                listOfPoints[iteration] = Point(width, height)
+
+    @staticmethod
+    def save_marked_area(listOfPoints, img):
+        '''
+        This method cuts out the marked part(s) of the image and save it to neg folder.
+
+        listOfPoints: list that contains Point objects
+        img: copy of the image
+        '''
+
+        for iteration in range(len(listOfPoints)):
+            if iteration % 2 == 1:
+                topLeftX = min(listOfPoints[iteration - 1].x,
+                               listOfPoints[iteration].x)
+                topLeftY = min(listOfPoints[iteration - 1].y,
+                               listOfPoints[iteration].y)
+
+                bottomRightX = max(listOfPoints[iteration - 1].x,
+                                   listOfPoints[iteration].x)
+                bottomRightY = max(listOfPoints[iteration - 1].y,
+                                   listOfPoints[iteration].y)
+
+                negativeImagesDir = os.path.join(
+                    os.getcwd(), 'detect_parking_lines', 'neg')
+
+                cv2.imwrite(os.path.join(
+                    negativeImagesDir, f'file{len(os.listdir(negativeImagesDir))}.jpg'), img=img[topLeftY:bottomRightY, topLeftX:bottomRightX])
+
+    @staticmethod
+    def draw_rectangles(listOfPoints, img):
+        '''
+        This method draws rectangles around marked objects.
+
+        listOfPoints: list that contains Point objects
+        img: copy of the image that you want to draw rectangles
+        '''
+
+        for iteration in range(len(listOfPoints)):
+            if iteration % 2 == 1:
+                firstPoint = (
+                    listOfPoints[iteration - 1].x, listOfPoints[iteration - 1].y)
+                secondPoint = (
+                    listOfPoints[iteration].x, listOfPoints[iteration].y)
+
+                image = cv2.rectangle(img=img, pt1=firstPoint,
+                                      pt2=secondPoint, color=(0, 0, 255), thickness=2)
+
+        return image
