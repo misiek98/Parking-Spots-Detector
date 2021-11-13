9ed16017096934d19bced79c182b6560ecded6de
misiek98 <michasiek098@gmail.com>
2021-11-13 13:33:33 +0100

    .

--- haar_model/tools/run_script.py
+++ haar_model/tools/run_script.py
@@ -4,15 +4,6 @@ import shutil
 from data_processing import Point, data_processing_methods
 
 
-listOfFolders = [os.path.join(os.getcwd(), 'haar_model', 'data'),
-                 os.path.join(os.getcwd(), 'haar_model', 'neg'),
-                 os.path.join(os.getcwd(), 'haar_model', 'pos'),
-                 os.path.join(os.getcwd(), 'haar_model', 'results')]
-
-for dir in listOfFolders:
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-
 listOfPoints = []
 
 
